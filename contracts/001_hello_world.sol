c8f369dfde8aa4959f4ae45a16b79441d44cc6ee
Nipun Jindal <mansapatiala@gmail.com>
2025-08-12 19:23:05 +0530

    adds constructor for hello world

--- contracts/001_hello_world.sol
+++ contracts/001_hello_world.sol
@@ -4,4 +4,11 @@ pragma solidity 0.8.26;
 
 contract HelloWorld {
     string public greet = "Hello World";
+    constructor(string memory _greet) {
+        greet = _greet;
+    }
+
+    function updateGreet(string memory _greet) public {
+        greet = _greet;
+    }
 }
