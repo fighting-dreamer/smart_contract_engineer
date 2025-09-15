c13b340f16ce38ac23377e23d6432fd47c3eaaf0
Nipun Jindal <mansapatiala@gmail.com>
2025-09-15 14:10:01 +0530

    testing hello worls with modifier

--- foundry-learning/HelloWorld/src/HelloWorld.sol
+++ foundry-learning/HelloWorld/src/HelloWorld.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.13;
 
 contract HelloWorld {
+    address private owner;
     string private message;
 
-    constructor() {}
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    constructor() {
+        owner = msg.sender;
+    }
 
-    function setMessage(string calldata _message) external returns(string memory) {
+    function setMessage(string calldata _message) external onlyOwner returns(string memory) {
         message = _message;
         return message;
     }
--- foundry-learning/HelloWorld/test/HelloWorld.t.sol
+++ foundry-learning/HelloWorld/test/HelloWorld.t.sol
@@ -16,4 +16,17 @@ contract HelloWorldTest is Test {
     function test_get() public view {
         assertEq(hw.get(), testMessage);
     }
+    function test_setMessageWithOwnerSuccess() public {
+        string memory someMessage = "someMessage";
+        hw.setMessage(someMessage);
+        assertEq(hw.get(), someMessage, "Owner should be able to set the message");
+    }
+
+    function test_setMessageWithNotOwnerFails() public {
+        address otherAccount = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+        string memory someMessage = "some Message";
+        vm.expectRevert();
+        vm.prank(otherAccount);
+        hw.setMessage(someMessage);
+    }
 }
