c7ac7daf2d05bd51c34986c440e6aef13727b695
Nipun Jindal <mansapatiala@gmail.com>
2025-09-15 13:26:15 +0530

    basic hello world contract

--- foundry-learning/HelloWorld/script/HelloWorld.s.sol
+++ foundry-learning/HelloWorld/script/HelloWorld.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {HelloWorld} from "../src/HelloWorld.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract HelloWorldScript is Script {
+    HelloWorld public hw;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        hw = new HelloWorld();
 
         vm.stopBroadcast();
     }
